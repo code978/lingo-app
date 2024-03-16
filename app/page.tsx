c9b8d9809f897e47c340fbbb7149e828c51d5090
Kunal Jain <56649142+code978@users.noreply.github.com>
2024-03-16 05:38:53 +0000

    added shadcn button.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1
-    >Lingo App</h1>
+
+    <>
+
+      <p className="text-green-500 text-3xl font-bold"
+      >Lingo App</p>
+      <Button variant={"destructive"} size={"lg"}>My Button</Button>
+
+    </>
   );
 }
